aa0813c2293ffb46c5491c9a323e62ab0cdb02b9
Yang Neiva <yangaraujo.neiva@gmail.com>
2022-10-26 17:29:14 -0300

    index page

---
+++ client/src/PostCreate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PostCreate = () => {
+    return <div>
+        <form>
+            <div className="form-group">
+                <label> Title </label>
+                <input className="form-control"/>
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    </div>;
+};
+
+export default PostCreate;
